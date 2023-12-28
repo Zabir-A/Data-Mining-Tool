b209d26ba70636df0e4192216fc9ae97f32409cb
Zabir Ahasan <zahasan1@my.bcit.ca>
2023-12-28 12:52:47 -0800

    Update requirements.txt & added main scheduler script to call the others timely

---
+++ main_scheduler.py
@@ -0,0 +1,26 @@
+import schedule
+import time
+import subprocess
+
+
+def run_scraper():
+    subprocess.run(["python", "scraper.py"])
+
+
+def run_check_db():
+    subprocess.run(["python", "check_db.py"])
+
+
+def run_to_discord():
+    subprocess.run(["python", "to_discord.py"])
+
+
+# Task Scheduling
+schedule.every().day.at("10:00").do(run_scraper)  # Runs daily at 10:00 AM
+schedule.every().sunday.at("20:00").do(run_check_db)  # Runs every Sunday at 8:00 PM
+schedule.every().day.at("15:00").do(run_to_discord)  # Runs daily at 3:00 PM
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)  # Wait one second before checking again
